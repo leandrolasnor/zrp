d1306d0203b17647f88188b80a3c0ba5ab2a0ee8
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-26 17:20:31 +0000

    feat: including enum on models classes

--- app/use_cases/list_heroes/model/hero.rb
+++ app/use_cases/list_heroes/model/hero.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ListHeroes::Model::Hero < ApplicationRecord
+  include Enums::Hero::Rank
 end
--- app/use_cases/show_hero/model/hero.rb
+++ app/use_cases/show_hero/model/hero.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ShowHero::Model::Hero < ApplicationRecord
+  include Enums::Hero::Rank
 end
