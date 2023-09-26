1f7c053dd03e0bf137f84c03ad66633a413b3f36
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-26 16:46:50 +0000

    chore(devise): config session store

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # ActionDispatch::Request::Session::DisabledSessionError
+    config.session_store(:cookie_store, key: '_interslice_session')
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
