dba43018311179f54068859cb352b6fda4c3be27
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-26 16:48:37 +0000

    feat(usecase): list heroes

---
+++ app/use_cases/list_heroes/model/hero.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class ListHeroes::Model::Hero < ApplicationRecord
+end
---
+++ app/use_cases/list_heroes/monad.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ListHeroes::Monad
+  include Dry::Monads[:result, :try]
+  include Dry.Types()
+  extend  Dry::Initializer
+
+  option :model, type: Interface(:page), default: -> { ListHeroes::Model::Hero }, reader: :private
+
+  def call(page: 1, per_page: 25)
+    Try { model.page(page).per(per_page).order(:desc) }
+  end
+end
