2ecf0cce17dade9a4c0b5b767c4b536fa6ebc06b
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-26 15:00:23 +0000

    chore: config devise and rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: proc { [200, {}, ['success']] }
+
+  scope '/v1' do
+    resources :heroes, only: [:show, :create, :update, :delete] do
+      get :list, on: :collection
+    end
+
+    get :tests, to: 'tests#test'
+  end
+
+  match '*path', via: :all, to: proc { [404, {}, nil] }
 end
