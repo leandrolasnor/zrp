f3716491914dc797c3cd938932bb8ea871c9a5e3
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-26 17:19:30 +0000

    fix: render id field on serializer

--- app/services/http/destroy_hero/serializer.rb
+++ app/services/http/destroy_hero/serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Http::DestroyHero::Serializer < ActiveModel::Serializer
-  attributes :name, :rank, :lat, :lng
+  attributes :id, :name, :rank, :lat, :lng
 end
