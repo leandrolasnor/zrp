ed917b7bbd8cbf6397bb343f43f1aac6fe5d55e7
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-27 02:41:42 +0000

    refactor: improving sneaker job

---
+++ app/workers/listeners_amqp/threats.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ListenersAmqp::Threats
+  include Sneakers::Worker
+
+  from_queue :threats
+
+  def work(threat)
+    parsed = JSON.parse(threat, symbolize_names: true)
+    created = Ws::CreateThreat::Service.(parsed)
+    ack! if created
+  end
+end
