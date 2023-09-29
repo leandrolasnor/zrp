89b4adabfd10891d1dce14c3820af8865d620668
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-29 04:53:21 +0000

    refactor: deallocate resource monad use case

--- app/use_cases/create_threat/monad.rb
+++ app/use_cases/create_threat/monad.rb
@@ -2,17 +2,20 @@
 
 class CreateThreat::Monad
   include Dry::Monads[:result, :try]
+  include Dry::Events::Publisher[:create_threat]
   include Dry.Types()
   extend  Dry::Initializer
+
+  register_event 'threat.created'
 
   option :model, type: Interface(:create), default: -> { CreateThreat::Model::Threat }, reader: :private
 
   def call(params)
     Try do
-      model.connection_pool.with_connection do
+      record = model.connection_pool.with_connection do
         model.create do
           _1.name = params[:monsterName] rescue nil
-          _1.rank = CreateThreat::Model::Threat.ranks[params[:dangerLevel].downcase] rescue nil
+          _1.rank = params[:dangerLevel].downcase rescue nil
           _1.lat = params[:location].first[:lat] rescue nil
           _1.lng = params[:location].first[:lng] rescue nil
           _1.status = CreateThreat::Model::Threat.statuses['problem'] unless (params[:location].first[:lng] rescue nil) &&
@@ -23,6 +26,8 @@ class CreateThreat::Monad
           _1.payload = params.to_json
         end
       end
+      publish('threat.created', threat: record) if record.enabled?
+      record
     end
   end
 end
