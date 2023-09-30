36c0a24b41020d35fce8f16aaaf2d40ddb93598d
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-30 08:01:49 +0000

    refactor: config enum on threat model

--- app/use_cases/deallocate_resource/model/threat.rb
+++ app/use_cases/deallocate_resource/model/threat.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DeallocateResource::Model::Threat < ApplicationRecord
+  include Enums::Threat::Status
+
   has_many :battles
   has_many :heroes, through: :battles
 end
