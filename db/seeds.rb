c57ede36bb30f6ad2ad02f5cce01287c25d9f5a4
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-09-30 20:58:27 +0000

    chore: registre 30 heroes on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-FactoryBot.create_list(:hero, 200)
+FactoryBot.create_list(:hero, 30)
