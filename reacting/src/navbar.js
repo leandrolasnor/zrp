f43c8bd04d82eb35d5b363fe3cd5c4c1eab1b55e
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-09 00:24:23 +0000

    feat: addon navbar on frontend

---
+++ reacting/src/navbar.js
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { Nav, Navbar, Button } from 'react-bootstrap'
+import { logout } from './auth/actions'
+
+const NavBar = () => {
+  const dispatch = useDispatch()
+  return(
+    <Navbar variant='pills'>
+      <Navbar.Brand href='#'>Dashboard</Navbar.Brand>
+      <Nav.Item>
+        <Nav.Link href='#heroes'>Heroes</Nav.Link>
+      </Nav.Item>
+      <Navbar.Collapse className='justify-content-end'>
+        <Nav.Item>
+          <Button variant='link' href='#' onClick={() => dispatch(logout())}>Sign out</Button>
+        </Nav.Item>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavBar
