12593c436aaeb6bdf658990f866bf8d37dea0ff5
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-09 22:41:47 +0000

    feat: addon dashboard with new insights

--- reacting/src/dashboard/component.js
+++ reacting/src/dashboard/component.js
@@ -1,34 +1,65 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import HeroesWorkingCount from './heroes_working_count.js'
+import HeroesWorking from './heroes_working.js'
 import { Row, Col } from 'react-bootstrap'
+import { TagGroup, Tag, Progress } from 'rsuite'
 
 const _ = require('lodash')
 
 const Dashboard = () => {
+  const [seconds, setSeconds] = useState(0)
   const dispatch = useDispatch()
   const { user } = useSelector(state => state.auth)
+  const authorization = _.get(user, 'authorization')
   const { metrics } = useSelector(state => state.metrics)
+  const average_time_to_match_hours = Number(_.get(metrics, ['average_time_to_match', 'hours'], 0)).toFixed(0)
+  const average_time_to_match_minutes = Number(_.get(metrics, ['average_time_to_match', 'minutes'], 0)).toFixed(0)
+  const average_time_to_match_seconds = Number(_.get(metrics, ['average_time_to_match', 'seconds'], 0)).toFixed(0)
+
+  const sse = () => {
+    setSeconds(0)
+    let eventSource = new EventSource(
+      `http://localhost:3000/v1/metrics/dashboard`,
+      { headers: {'authorization': authorization} }
+    )
+    eventSource.onmessage = (e) => dispatch(JSON.parse(e.data))
+    eventSource.onerror = (e) => eventSource.close()
+  }
 
   const initialized = useRef(false)
-
   useEffect(() => {
     if (!initialized.current) {
       initialized.current = true
-      let eventSource = new EventSource(
-        `http://localhost:3000/v1/metrics/dashboard`,
-        { headers: {'authorization': _.get(user, 'authorization')} }
-      )
-      eventSource.onmessage = (e) => dispatch(JSON.parse(e.data))
-      eventSource.onerror = (e) => eventSource.close()
+      sse()
+      setInterval(() => {
+        setSeconds(prev => prev+12)
+      },1000)
+      const sse_interval = setInterval(sse, 10000)
     }
-  }, [dispatch, user, metrics])
+  }, [])
 
   return(
     <Row>
-      <Col sm={4}>
-        <HeroesWorkingCount />
-      </Col>
+      <Row>
+        <Progress.Line status="success" strokeWidth={1} percent={seconds} showInfo={false} />
+        <Col sm={12}>
+          <TagGroup>
+            <Tag color="blue" size="sm">average score :: {`${Number(_.get(metrics, 'average_score', 0)).toFixed(2)}`}</Tag>
+            <Tag color="green" size="sm">
+              average time to match :: {
+                `${average_time_to_match_hours > 0 ? `${average_time_to_match_hours} hours` : ``}
+                ${average_time_to_match_minutes > 0 ? `${average_time_to_match_minutes} minutes` : ``}
+                ${average_time_to_match_seconds > 0 ? `${average_time_to_match_seconds} seconds` : ``}`
+              }
+            </Tag>
+          </TagGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='mt-2' sm={4}>
+          <HeroesWorking metrics={metrics} />
+        </Col>
+      </Row>
     </Row>
   )
 }
---
+++ reacting/src/dashboard/heroes_working.js
@@ -0,0 +1,90 @@
+import { Row, Col, Card } from 'react-bootstrap'
+import { FlexboxGrid, Tag, Progress } from 'rsuite'
+import { styled } from 'styled-components'
+
+const _ = require('lodash')
+
+const StyledCircleProgress = styled(Progress.Circle)`
+  width: 120,
+  display: 'inline-block',
+  marginRight: 10
+`;
+
+const HeroesWorking = props => {
+  const { metrics } = props
+  const hero_count = () => _.get(metrics, 'hero_count', 0)
+  const heroes_working_count = () => Object.keys(_.get(metrics, 'heroes_grouped_rank_status', {})).reduce(
+    (sum,key)=> {
+      if(JSON.stringify(key).match(/working/)) return sum+parseFloat(_.get(metrics, ['heroes_grouped_rank_status', `${key}`], 0)||0)
+      return sum
+    }, 0
+  )
+
+  const heroes_s_working_count = () => Object.keys(_.get(metrics, 'heroes_grouped_rank_status', {})).reduce(
+    (sum,key)=> {
+      if(JSON.parse(key).join('-').match(/s-working/)) return sum+parseFloat(_.get(metrics, ['heroes_grouped_rank_status', `${key}`], 0)||0)
+      return sum
+    }, 0
+  )
+  const heroes_a_working_count = () => Object.keys(_.get(metrics, 'heroes_grouped_rank_status', {})).reduce(
+    (sum,key)=> {
+      if(JSON.parse(key).join('-').match(/a-working/)) return sum+parseFloat(_.get(metrics, ['heroes_grouped_rank_status', `${key}`], 0)||0)
+      return sum
+    }, 0
+  )
+  const heroes_b_working_count = () => Object.keys(_.get(metrics, 'heroes_grouped_rank_status', {})).reduce(
+    (sum,key)=> {
+      if(JSON.parse(key).join('-').match(/b-working/)) return sum+parseFloat(_.get(metrics, ['heroes_grouped_rank_status', `${key}`], 0)||0)
+      return sum
+    }, 0
+  )
+  const heroes_c_working_count = () => Object.keys(_.get(metrics, 'heroes_grouped_rank_status', {})).reduce(
+    (sum,key)=> {
+      if(JSON.parse(key).join('-').match(/c-working/)) return sum+parseFloat(_.get(metrics, ['heroes_grouped_rank_status', `${key}`], 0)||0)
+      return sum
+    }, 0
+  )
+  const heroes_s_count = () => _.get(metrics , ['heroes_grouped_rank', 's'])
+  const heroes_a_count = () => _.get(metrics , ['heroes_grouped_rank', 'a'])
+  const heroes_b_count = () => _.get(metrics , ['heroes_grouped_rank', 'b'])
+  const heroes_c_count = () => _.get(metrics , ['heroes_grouped_rank', 'c'])
+
+  const heroes_working_percent = () => (heroes_working_count() / hero_count()) * 100
+  const heroes_s_working_percent = () => (heroes_s_working_count() / heroes_s_count()) * 100
+  const heroes_a_working_percent = () => (heroes_a_working_count() / heroes_a_count()) * 100
+  const heroes_b_working_percent = () => (heroes_b_working_count() / heroes_b_count()) * 100
+  const heroes_c_working_percent = () => (heroes_c_working_count() / heroes_c_count()) * 100
+
+  return(
+    <Card>
+      <Card.Body>
+        <Card.Title>Heroes</Card.Title>
+        <Card.Subtitle className="mb-0 text-muted">{`${heroes_working_count() || 0} Working`}</Card.Subtitle>
+        <Row>
+          <Col className='mt-4' sm={5}>
+            <StyledCircleProgress strokeWidth={6} percent={Number(heroes_working_percent().toFixed(0))} strokeColor="#395463" />
+          </Col>
+          <Col sm={1} className='ms-3'>
+            <Progress.Line strokeWidth={22} vertical percent={Number(heroes_s_working_percent().toFixed(0))} status="active" strokeColor="#2986cc" />
+            <Tag color="blue">S</Tag>
+          </Col>
+          <Col sm={1} className='ms-3' >
+            <Progress.Line strokeWidth={22} vertical percent={Number(heroes_a_working_percent().toFixed(0))} status="active" strokeColor="#0AB653" />
+            <Tag color="green">A</Tag>
+          </Col>
+          <Col sm={1} className='ms-3'>
+            <Progress.Line strokeWidth={22} vertical percent={Number(heroes_b_working_percent().toFixed(0))} status="active" strokeColor="#a442f5" />
+            <Tag color="violet">B</Tag>
+          </Col>
+          <Col sm={1} className='ms-3'>
+            <Progress.Line strokeWidth={22} vertical percent={Number(heroes_c_working_percent().toFixed(0))} status="active" strokeColor="#ff0000" />
+            <Tag color="red">C</Tag>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  )
+}
+
+
+export default HeroesWorking
