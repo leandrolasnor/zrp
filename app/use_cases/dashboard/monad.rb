e73c0bc3c8a35a5f194c21371583c4c7b3fe6105
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-11 02:27:37 +0000

    refactor: removing scatter metrics and addon battle_count

--- app/use_cases/dashboard/monad.rb
+++ app/use_cases/dashboard/monad.rb
@@ -19,6 +19,10 @@ class Dashboard::Monad
       metrics << [:threat_count, threat.not_problem.count]
       publish('metrics.fetched', payload: [metrics.last].to_h)
 
+      battle_count = threat.not_problem.not_enabled.count
+      metrics << [:battle_count, battle_count]
+      publish('metrics.fetched', payload: [metrics.last].to_h)
+
       metrics << [:hero_count, hero.not_disabled.count]
       publish('metrics.fetched', payload: [metrics.last].to_h)
 
@@ -39,27 +43,13 @@ class Dashboard::Monad
 
       battles_grouped_two_heroes = battle.group(:threat_id).having('count(*) = 2').count
       battles_two_heroes_count = battles_grouped_two_heroes.count
-      battle_count = metrics.to_h[:threat_count]
       battles_one_hero_count = battle_count - battles_two_heroes_count
       battles_two_and_one_percent = [
-        ['One', ((battles_one_hero_count.to_f / battle_count) * 100).round(2)],
-        ['Two', ((battles_two_heroes_count.to_f / battle_count) * 100).round(2)]
+        ['One Hero', ((battles_one_hero_count.to_f / battle_count) * 100).round(2)],
+        ['Two Heroes', ((battles_two_heroes_count.to_f / battle_count) * 100).round(2)]
       ]
       metrics << [:battles_two_and_one_percent, battles_two_and_one_percent]
       publish('metrics.fetched', payload: [metrics.last].to_h)
-
-      # threats_grouped_score_rank = threat.not_problem.not_enabled.joins(:battles).group(:'battles.score', :rank).count
-      # threats_god_grouped_score_rank = threats_grouped_score_rank.select { _1.second == 'god' }
-      # threats_dragon_grouped_score_rank = threats_grouped_score_rank.select { _1.second == 'dragon' }
-      # threats_tiger_grouped_score_rank = threats_grouped_score_rank.select { _1.second == 'tiger' }
-      # threats_wolf_grouped_score_rank = threats_grouped_score_rank.select { _1.second == 'wolf' }
-
-      # metrics = << [
-      #   :matches_scatter,
-      #   [
-
-      #   ]
-      # ]
 
       metrics << [
         :average_time_to_match,
