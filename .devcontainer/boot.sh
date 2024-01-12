2f156c4677b72a65401fcc7dae11fcbf33698635
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-12 15:16:48 +0000

    chore: addon start.sh without workdir

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
-rm -f /app/tmp/pids/server.pid
-git pull
 bundle exec rake db:migrate
 bundle exec rake db:seed
 foreman start
---
+++ .devcontainer/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+rm -f tmp/pids/server.pid
+git pull
+
