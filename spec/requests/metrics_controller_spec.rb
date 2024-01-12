fb903d7420c4152c27a4be82f074a9f3143d17e4
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-12 18:47:54 +0000

    test: addon metrics on test case

--- spec/requests/metrics_controller_spec.rb
+++ spec/requests/metrics_controller_spec.rb
@@ -13,19 +13,12 @@ RSpec.describe MetricsController do
         let(:threats) { create_list(:threat, 7, :dashboard) }
         let(:url) { 'fake.com/stream' }
         let(:client) { SSE::EventSource.new(url) }
-        let(:expected_body) do
-          {
-            data: {
-              type: 'METRICS_FETCHED',
-              payload: []
-            }
-          }
-        end
 
         before do
           heroes.first.working!
           heroes.second.working!
-
+          create(:battle, score: 80, hero_id: heroes.first.id, threat_id: threats.first.id, finished_at: 5.second.from_now)
+          create(:battle, score: 74, hero_id: heroes.second.id, threat_id: threats.second.id, finished_at: 5.second.from_now)
           threats.first.disabled!
           threats.second.disabled!
           threats.third.problem!
@@ -47,12 +40,17 @@ RSpec.describe MetricsController do
 
             expect(response).to have_http_status(:ok)
             expect(response.headers['Content-Type']).to eq('text/event-stream')
-            expect(events.count).to eq(5)
+            expect(events.count).to eq(10)
             expect(events.first['payload'].keys.first).to eq('threat_count')
-            expect(events.second['payload'].keys.first).to eq('hero_count')
-            expect(events.third['payload'].keys.first).to eq('threats_grouped')
-            expect(events.fourth['payload'].keys.first).to eq('heroes_grouped')
-            expect(events.fifth['payload'].keys.first).to eq('average_score')
+            expect(events.second['payload'].keys.first).to eq('battle_count')
+            expect(events.third['payload'].keys.first).to eq('hero_count')
+            expect(events.fourth['payload'].keys.first).to eq('threats_grouped_rank_status')
+            expect(events.fifth['payload'].keys.first).to eq('threats_grouped_rank')
+            expect(events[5]['payload'].keys.first).to eq('heroes_grouped_rank_status')
+            expect(events[6]['payload'].keys.first).to eq('heroes_grouped_rank')
+            expect(events[7]['payload'].keys.first).to eq('average_score')
+            expect(events[8]['payload'].keys.first).to eq('battles_two_and_one_percent')
+            expect(events[9]['payload'].keys.first).to eq('average_time_to_match')
           end
         end
       end
