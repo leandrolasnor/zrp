0521d0c6fcb2207614f2ebf9a3a7d972c3c29742
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-13 17:48:25 +0000

    chore: sign in container as root user

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
-chmod +x ./.devcontainer/start.sh
-bundle
-yarn --cwd ./reacting install
-yarn --cwd ./socket.io/server install
-yarn --cwd ./socket.io/client install
 bundle exec rake db:migrate
 bundle exec rake db:seed
 foreman start
