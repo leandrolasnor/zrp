ec2dfb162f8db623ad272b74465b89d89272c9b6
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-13 22:24:42 +0000

    build: install dependence on build and workspace

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
+yarn --cwd ./reacting install
+yarn --cwd ./socket.io/server install
+yarn --cwd ./socket.io/client install
+bundle
 bundle exec rake db:migrate
 bundle exec rake db:seed
