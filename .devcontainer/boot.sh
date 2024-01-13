dbad8e491a853175be8d3c43fa26ad806e1d8f5a
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-13 07:49:43 +0000

    chore: improving devcontainer

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -2,6 +2,8 @@
 chmod +x ./.devcontainer/start.sh
 bundle
 yarn --cwd ./reacting install
+yarn --cwd ./socket.io/server install
+yarn --cwd ./socket.io/client install
 bundle exec rake db:migrate
 bundle exec rake db:seed
-foreman start -f Procfile.api
+foreman start
