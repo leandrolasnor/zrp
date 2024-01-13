32973912d0c6eb2facd5478cab8a68be68ef2c37
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-13 18:40:38 +0000

    chore: do install dependences only on boot

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
+bundle
+yarn --cwd ./reacting install
+yarn --cwd ./socket.io/server install
+yarn --cwd ./socket.io/client install
 bundle exec rake db:migrate
 bundle exec rake db:seed
 foreman start
