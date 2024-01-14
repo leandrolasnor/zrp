af306837eb238f378f8e46076257cbe0a243ab75
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-14 18:41:08 +0000

    chore: exec the container's main process

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
+set -e
 yarn --cwd ./reacting install
 yarn --cwd ./socket.io/server install
 yarn --cwd ./socket.io/client install
 bundle
 bundle exec rake db:migrate
 bundle exec rake db:seed
+exec "$@"
