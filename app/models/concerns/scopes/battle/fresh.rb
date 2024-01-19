00a4d6f53a6886884ca4b0a09f1e2da1f55fab45
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-19 17:12:30 +0000

    feat: fresh scope for battle and threat more recent

---
+++ app/models/concerns/scopes/battle/fresh.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Scopes::Battle::Fresh
+  extend ActiveSupport::Concern
+
+  included do
+    scope :fresh, -> { where(created_at: 45.minutes.ago...::Time.zone.now) }
+  end
+end
