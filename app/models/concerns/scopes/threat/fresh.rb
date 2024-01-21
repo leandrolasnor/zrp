4e144abaec349984f3bbbf66deefe2a3d35b919b
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-01-21 18:40:32 -0300

    fix: use inclusive interval

--- app/models/concerns/scopes/threat/fresh.rb
+++ app/models/concerns/scopes/threat/fresh.rb
@@ -4,6 +4,6 @@ module Scopes::Threat::Fresh
   extend ActiveSupport::Concern
 
   included do
-    scope :fresh, -> { where(created_at: 45.minutes.ago...::Time.zone.now) }
+    scope :fresh, -> { where(created_at: 45.minutes.ago..::Time.zone.now) }
   end
 end
