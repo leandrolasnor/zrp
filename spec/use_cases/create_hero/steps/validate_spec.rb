5fb2de308bd0c292873dbd89b88dbacd57862935
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-02-10 05:13:58 +0000

    test(use case): create hero

---
+++ spec/use_cases/create_hero/steps/validate_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe CreateHero::Steps::Validate do
+  describe '.call' do
+    let(:call) { subject.(params) }
+
+    context 'on Success' do
+      let(:params) do
+        {
+          name: 'Hero',
+          rank: 0,
+          lat: 123.90,
+          lng: -89.345
+        }
+      end
+
+      it 'must be able to validate hero params' do
+        expect(call).to be_success
+      end
+    end
+
+    context 'on Failure' do
+      let(:params) do
+        {
+          name: 1,
+          rank: -1,
+          lat: 'hero',
+          lng: 'rank'
+        }
+      end
+      let(:expected_errors) do
+        {
+          name: ['must be a string'],
+          rank: ['must be one of: 0, 1, 2, 3'],
+          lat: ['must be a float'],
+          lng: ['must be a float']
+        }
+      end
+
+      it 'must be able to get errors from contract' do
+        expect(call).to be_failure
+        expect(call.failure.errors.to_h).to match(expected_errors)
+      end
+    end
+  end
+end
