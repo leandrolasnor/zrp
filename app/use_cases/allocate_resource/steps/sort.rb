7dff88657bf2bdda1bd9d49c1c208a5a9ede6235
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-03-08 15:12:35 +0000

    refactor: code design

--- app/use_cases/allocate_resource/steps/sort.rb
+++ app/use_cases/allocate_resource/steps/sort.rb
@@ -4,6 +4,6 @@ class AllocateResource::Steps::Sort
   include Dry::Monads[:result]
 
   def call(matches)
-    matches.sort_by! { _1.score! }.reverse! # sort_by{...}.reverse is fastest
+    matches.sort_by!(&:score!).reverse! # sort_by{...}.reverse is fastest
   end
 end
