4cbf04ce8903666511d770be5620ddc59c769b1a
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-03-10 14:02:49 +0000

    test: change class name on create threat service

--- spec/services/ws/create_threat/listeners/allocate_resource/job_spec.rb
+++ spec/services/ws/create_threat/listeners/allocate_resource/job_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe Ws::CreateThreat::AllocateResource::Job do
+RSpec.describe Ws::CreateThreat::Listeners::AllocateResource::Job do
   let(:call) { described_class.new.call(threat.id) }
   let(:threat) { create(:threat, :allocate_resource) }
 
