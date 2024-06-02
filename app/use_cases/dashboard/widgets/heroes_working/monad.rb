cdcf6fd67fb24f2a6cad8f4efdf9547fc0675098
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-06-02 04:04:51 +0000

    refactor: improve calcule and decimals

--- app/use_cases/dashboard/widgets/heroes_working/monad.rb
+++ app/use_cases/dashboard/widgets/heroes_working/monad.rb
@@ -13,11 +13,11 @@ class Dashboard::Widgets::HeroesWorking::Monad
       heroes_working = model.ms_raw_search('', page: 0, facets: [:rank], filter: ['status = working'])
 
       {
-        global: (heroes_working["totalHits"] / heroes["totalHits"].to_f * 100).round(2),
-        s: (heroes_working["facetDistribution"]["rank"]["s"] / heroes["facetDistribution"]["rank"]["s"].to_f * 100).round(2),
-        a: (heroes_working["facetDistribution"]["rank"]["a"] / heroes["facetDistribution"]["rank"]["a"].to_f * 100).round(2),
-        b: (heroes_working["facetDistribution"]["rank"]["b"] / heroes["facetDistribution"]["rank"]["b"].to_f * 100).round(2),
-        c: (heroes_working["facetDistribution"]["rank"]["c"] / heroes["facetDistribution"]["rank"]["c"].to_f * 100).round(2),
+        global: (heroes_working["totalHits"].to_f / heroes["totalHits"] * 100).round(0),
+        s: (heroes_working["facetDistribution"]["rank"]["s"].to_f / heroes["facetDistribution"]["rank"]["s"] * 100).round(0),
+        a: (heroes_working["facetDistribution"]["rank"]["a"].to_f / heroes["facetDistribution"]["rank"]["a"] * 100).round(0),
+        b: (heroes_working["facetDistribution"]["rank"]["b"].to_f / heroes["facetDistribution"]["rank"]["b"] * 100).round(0),
+        c: (heroes_working["facetDistribution"]["rank"]["c"].to_f / heroes["facetDistribution"]["rank"]["c"] * 100).round(0),
         count: heroes_working["totalHits"]
       }
     end
