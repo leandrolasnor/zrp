0be12ac7829d2523a98e654f61412f0b7aab0bbb
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-06-02 06:22:58 +0000

    test: improve case test for allocate resource job

--- spec/services/ws/create_threat/listeners/allocate_resource/job_spec.rb
+++ spec/services/ws/create_threat/listeners/allocate_resource/job_spec.rb
@@ -57,9 +57,6 @@ RSpec.describe Ws::CreateThreat::Listeners::AllocateResource::Job do
     before do
       allow(Resque).
         to receive(:enqueue_at).
-        with(duck_type(:to_time), Ws::CreateThreat::Listeners::Dashboard::Widgets::HeroesWorking::Job)
-      allow(Resque).
-        to receive(:enqueue_at).
         with(duck_type(:to_time), Ws::CreateThreat::Listeners::DeallocateResource::Job, threat.id)
       heroes
       call
@@ -69,9 +66,6 @@ RSpec.describe Ws::CreateThreat::Listeners::AllocateResource::Job do
       expect(Resque).
         to have_received(:enqueue_at).
         with(duck_type(:to_time), Ws::CreateThreat::Listeners::DeallocateResource::Job, threat.id)
-      expect(Resque).
-        to have_received(:enqueue_at).
-        with(duck_type(:to_time), Ws::CreateThreat::Listeners::Dashboard::Widgets::HeroesWorking::Job)
     end
   end
 end
