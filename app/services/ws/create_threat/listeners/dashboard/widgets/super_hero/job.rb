19e35d8fb20ce7dbe0b3f0698aef4454a480abc3
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-06-23 15:47:25 +0000

    feat: listener and job for super hero widget

---
+++ app/services/ws/create_threat/listeners/dashboard/widgets/super_hero/job.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Ws::CreateThreat::Listeners::Dashboard::Widgets::SuperHero::Job
+  include Dry.Types()
+  extend Dry::Initializer
+
+  option :monad, type: Interface(:call), default: -> { Dashboard::Widgets::SuperHero::Monad.new }, reader: :private
+  option :event, type: Dry::Types['string'], default: -> { 'WIDGET_SUPER_HERO_FETCHED' }, reader: :private
+  option :identifier, type: Dry::Types['string'], default: -> { 'token' }, reader: :private
+  option :broadcast,
+         type: Instance(Proc),
+         default: -> { proc { ActionCable.server.broadcast(identifier, { type: event, payload: _1 }) } },
+         reader: :private
+
+  def call
+    res = monad.()
+    broadcast.(res.value!) if res.success?
+    Rails.logger.error(res.exception) if res.failure?
+  end
+
+  @queue = :widget_super_hero
+  def self.perform
+    new.call
+  end
+end
