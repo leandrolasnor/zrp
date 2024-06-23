1e99cbdb9918c119a56bc59760a2622e14e755aa
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-06-23 15:48:42 +0000

    feat: use case for super hero widget

---
+++ app/use_cases/dashboard/widgets/super_hero/monad.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Dashboard::Widgets::SuperHero::Monad
+  include Dry::Monads[:try]
+  include Dry.Types()
+  extend Dry::Initializer
+
+  option :model, type: Interface(:ms_raw_search), default: -> { Dashboard::Model::Hero }, reader: :private
+
+  def call
+    Try do
+      name, rank = model.includes(:battles)
+        .where('battles.finished_at': 20.minutes.ago...::Time.zone.now)
+        .group(:name, :rank).sum(:score).max_by(&:second)&.first
+      { name: name, rank: rank }
+    end
+  end
+end
