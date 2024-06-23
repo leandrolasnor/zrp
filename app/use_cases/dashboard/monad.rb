2fd48c78c50a8ef402a7a91f108463c63794f884
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-06-23 15:49:10 +0000

    refactor: use case dashboard

--- app/use_cases/dashboard/monad.rb
+++ app/use_cases/dashboard/monad.rb
@@ -8,53 +8,31 @@ class Dashboard::Monad
 
   register_event('metrics.fetched')
 
-  option :threat, type: Interface(:find), default: -> { Dashboard::Model::Threat }, reader: :private
-  option :hero, type: Interface(:find), default: -> { Dashboard::Model::Hero }, reader: :private
-  option :battle, type: Interface(:find), default: -> { Dashboard::Model::Battle }, reader: :private
-  option :duration, type: Instance(Proc), default: -> { proc { ActiveSupport::Duration.build(_1).parts } }
+  option :metrics,
+         type: Array,
+         default: -> {
+                    [
+                      Dashboard::Widgets::SuperHero::Monad,
+                      Dashboard::Widgets::AverageScore::Monad,
+                      Dashboard::Widgets::AverageTimeToMatch::Monad,
+                      Dashboard::Widgets::HeroesWorking::Monad,
+                      Dashboard::Widgets::ThreatsDisabled::Monad,
+                      Dashboard::Widgets::BattlesLineup::Monad
+                    ]
+                  },
+         private: :reader
 
   def call
     Try do
-      metrics = []
+      result = metrics.map do
+        name = _1.name.split('::').third.underscore.to_sym
+        res = _1.new.()
+        payload = res.success? ? [name, res.value!] : [name, nil]
+        publish('metrics.fetched', payload: [payload].to_h) if payload.second.present?
+        payload
+      end
 
-      metrics << [
-        :average_score,
-        Rails.cache.fetch('average_score', expires_in: 1.minute) do
-          search = battle.ms_raw_search(
-            '',
-            page: 1,
-            hits_per_page: 300,
-            filter: ["finished_at > #{20.minutes.ago.to_time.to_i}"]
-          )
-          hits = search['hits']
-          sum_score = hits.reduce(0.0) { |sum, i| sum + i['score'].to_f }
-          (sum_score / search['totalHits']).round(2)
-        end
-      ]
-      publish('metrics.fetched', payload: [metrics.last].to_h)
-
-      metrics << [
-        :average_time_to_match,
-        Rails.cache.fetch('average_time_to_match', expires_in: 30.seconds) do
-          duration.(threat.fresh.disabled
-            .includes(:battles)
-            .limit(25)
-            .average('battles.created_at - threats.created_at'))
-        end
-      ]
-      publish('metrics.fetched', payload: [metrics.last].to_h)
-
-      metrics << [
-        :super_hero,
-        Rails.cache.fetch('super_hero', expires_in: 30.seconds) do
-          name, rank = hero.includes(:battles)
-            .where('battles.finished_at': 20.minutes.ago...::Time.zone.now)
-            .group(:name, :rank).sum(:score).max_by(&:second)&.first
-          { name: name, rank: rank }
-        end
-      ]
-      publish('metrics.fetched', payload: [metrics.last].to_h)
-      metrics.to_h
+      result.to_h
     end
   end
 end
