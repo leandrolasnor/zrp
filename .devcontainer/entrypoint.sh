d74de0784dab48069f275968af1239b706989386
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2025-08-09 16:10:55 -0300

    🔧 chore(devcontainer): refactor pgAdmin configuration
    - update pgAdmin environment variables for clarity and consistency
    - remove custom entrypoint and server configuration template
    - use default pgAdmin setup with environment variables for server configuration

--- .devcontainer/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Substitui variáveis no template e gera o servers.json final
-envsubst < /pgadmin4/servers.json.template > /pgadmin4/servers.json
-
-# Executa o entrypoint original do pgAdmin
-exec /entrypoint.sh "$@"
