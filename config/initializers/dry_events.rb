f92ef2703c2be77db8394cc758b36528118e3911
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2026-06-28 10:59:33 -0300

    ♻️ refactor(events): extract events constant - move EVENTS constant from AppEvents class to top-level initializer - improve global accessibility of event names within initializer

--- config/initializers/dry_events.rb
+++ config/initializers/dry_events.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+EVENTS = %w[
+  insufficient_resources resource_allocated resource_not_allocated
+  resource_deallocated threat_created hero_removed_from_index
+].freeze
+
 Rails.configuration.to_prepare do
   Dry::Events::Publisher.registry.clear
 end
@@ -11,15 +16,10 @@ class AppEvents
   def self.publish(...) = self.instance.publish(...)
   def self.subscribe(...) = self.instance.subscribe(...)
 
-  EVENTS = %W[
-    insufficient_resources resource_allocated resource_not_allocated
-    resource_deallocated threat_created hero_removed_from_index
-  ].freeze
-
   EVENTS.each { register_event it }
 end
 
-AppEvents::EVENTS.each do |event_name|
-  klass = "Subscribers::#{event_name.camelize}".constantize
+EVENTS.each do |event_name|
+  klass = "#{event_name.camelize}::Subscriber".constantize
   AppEvents.subscribe(event_name) { |event| klass.call(event) }
 end
